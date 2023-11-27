14c2c72feac80093c18a8b2ff7a8563b94f32895
Vitor-Augusto03 <vitoraugustoalves2016@gmail.com>
2023-11-27 13:12:15 -0300

    Gerenciador

--- src/componenteslogin/Input/index.js
+++ src/componenteslogin/Input/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ type, placeholder, value, onChange }) => {
   return (
     <Input
-      className="outline-none W-full  pt-4 pr-3 rounded text-base border-none bg-blue-900 "
+    className="input-login"
       value={value}
       onChange={onChange}
       type={type}
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -7,50 +7,70 @@ import Logo from "../../logo.svg";
 import { useEffect, useState } from "react";
 
 const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (email & senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+    const res = signin (email, senha);
+
+    if (res) {
+      setError(res);
+      return
+    }
+    navigate("/home");
+  };
+
   return (
-    <div className="bg-gradient-to-b from-blue-950 to-slate-900">
+    <body className="bg-gradient-to-b from-blue-950 to-slate-900 h-screen">
       {" "}
-      <div className="text-center font-bold text-lg py-3 bg-blue-500 w-screen mb-36">
+      <div className="text-center font-extrabold text-3xl py-3   text-white">
         <label>Taks Manager</label>
       </div>
       <section
         className="py-56 px-8 text-center flex items-stretch mx-auto"
         style={{ maxWidth: 700 }}
       >
-        <div className="bg-sky-400 p-10 rounded-l ">
+        <div className="bg-blue-900 p-10 rounded-l">
           <img
             src={Logo}
-            width={450}
+            width={500}
             alt="Logo"
             className=" hover:animate-[spin_4s_linear_infinite] cursor-pointer "
           />
         </div>
-        <div className="bg-slate-900  rounded-r px-10 grow flex flex-col justify-center shadow-lg shadow-slate text-slate-50 font-bold">
-        <h1 className="text-2xl font-bold duration-500">Acesse a sua conta</h1>
+        <div className="bg-slate-900  rounded-r px-10 grow flex flex-col justify-center shadow-lg shadow-slate text-slate-50 font-bold pr-16">
+          <h1 className="text-xl font-bold duration-500">Acesse a sua conta</h1>
           <p className="pt-10 text-base text-left text-slate-50">Email</p>
-          <input
-            className="py-1 mb-5 pr-4 bg-slate-800 text-base rounded focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-red-500 invalid:text-red-600
-            focus:invalid:red-pink-500 focus:invalid:ring-red-500"
+          <Input
             type="email"
             value={email}
             onChange={(e) => [setEmail(e.target.value), setError("")]}
           />
           <p className="text-base text-left text-slate-50"> Senha</p>
-          <input
-            className=" py-1 text-base rounded bg-slate-800  text-slate-50 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 
-            invalid:border-red-500 invalid:text-red-600
-            focus:invalid:red-pink-500 focus:invalid:ring-red-500"
+          <Input
             type="password"
             value={senha}
-            onChange={(e) => [setSenha(setSenha(e.target.value), setError(""))]}
+            onChange={(e) => [setSenha(e.target.value), setError("")]}
           />
         </div>
       </section>
       <label>{error}</label>
-    </div>
+      <Button text="Entrar" onClick={handleLogin} />
+      <label>
+        Não tem uma conta?
+        <strong>
+          <Link to="/signup">&nbsp; Registre-se</Link>
+        </strong>
+      </label>
+    </body>
   );
 };
 export default Signin;
